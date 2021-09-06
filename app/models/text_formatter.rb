6823cd2556c6a923ae77a774d5a5071e3b0cf9de
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-09-06 11:58:23 -0400

    change map to each and grab author_name in create_paragraphs

--- app/models/text_formatter.rb
+++ app/models/text_formatter.rb
@@ -9,10 +9,12 @@ class TextFormatter < ApplicationRecord
 
     def self.create_paragraphs(text_file)
         sections_split = text_file.split(/\n\n/)
-        array_of_sections = sections_split.map do |section|
+        array_of_sections = []
+        sections_split.each do |section|
             case section
             when /AUTHOR/
                 # method to set author for quotes: set_author(section)
+                author_name = section.split(": ")[1]
             when /TITLE/
                 # method to set title for quotes set_title(section)
             when /TRANSLATOR/
@@ -20,12 +22,16 @@ class TextFormatter < ApplicationRecord
             when /BOOK/
                 set_book_number(section)
             else
-                section
+                array_of_sections << section
             end
         end
         array_of_sections.compact
     end
 
+    def self.find_or_create_author_by_name(name)
+        author = (Author.find_by(name: name) || Author.create(name: name))
+    end
+    
     def self.set_book_number(book_number)
         # Quote.new's quote instance addes book_number attribute of the current Book_number
     end
