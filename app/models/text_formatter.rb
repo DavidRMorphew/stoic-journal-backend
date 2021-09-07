87794986612f5eaaac4b9d4ad130a74e50f9c5f9
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-09-06 23:31:59 -0400

    add section_num incrementally with reset at book num change

--- app/models/text_formatter.rb
+++ app/models/text_formatter.rb
@@ -3,35 +3,27 @@ class TextFormatter < ApplicationRecord
     # meditations = File.open("./texts/marcus_aurelius_meditations.txt")
     # new_file = meditations.read
 
-    attr_accessor :current_book_number
-
-    @@current_book_number = ""
-
     def self.create_paragraphs(text_file)
         sections_split = text_file.split(/\n\n/)
-        array_of_sections = []
         sections_split.each do |section|
             case section
             when /AUTHOR/
-                # method to set author for quotes: set_author(section)
                 author_name = section.split(": ")[1]
                 @@author = self.find_or_create_author_by_name(author_name)
             when /TITLE/
-                # method to set title for quotes set_title(section)
                 work_title = section.split(": ")[1]
                 @@work = self.find_or_create_work_by_title(work_title)
             when /TRANSLATOR/
-                # method to set translator for quotes set_translator(section)
                 translator_name = section.split(": ")[1]
                 @@translator = self.find_or_create_translator_by_name(translator_name)
-                binding.pry
             when /BOOK/
                 @@book = section.split(" ")[1]
+                @@section_num = 1
             else
-                array_of_sections << section
+                @@work.quotes.create(body: section, translator: @@translator, book_num: @@book, section_num: @@section_num)
+                @@section_num += 1
             end
         end
-        array_of_sections.compact
     end
 
     def self.find_or_create_author_by_name(name)
@@ -47,5 +39,4 @@ class TextFormatter < ApplicationRecord
         last_name = name.split(" ")[1]
         translator = (Translator.find_by(first_name: first_name, last_name: last_name) || Translator.create(first_name: first_name, last_name: last_name))
     end
-
 end
