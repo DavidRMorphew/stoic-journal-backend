abd6798d8b9c5641a4ea8c5b2c588e5101841a6d
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-09-13 19:11:42 -0400

    remove created_at, updated_at from index render

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,7 @@ class QuotesController < ApplicationController
   
   def index
     @quotes = Quote.all
-    render json: @quotes
+    render json: @quotes, except: [:created_at, :updated_at]
   end
 
 end
