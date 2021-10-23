fd1bf9a91e7e62ed5e5ad91a4ec84d9985b3b14a
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-23 16:07:50 -0400

    add author model test for name attribute presence

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "requires the presence of a name" do
+    expect(Author.new).not_to be_valid
+  end
 end
