646119cd7c440e4d5dc955fa67d16e3a92e347fb
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-23 19:55:37 -0400

    add author has many works validation test

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,6 @@
 class Work < ApplicationRecord
     validates :title, presence: true
-    belongs_to :author
+    # belongs_to :author
     has_many :quotes
     has_many :translators, through: :quotes
 end
