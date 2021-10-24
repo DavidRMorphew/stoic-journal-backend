16001ac220cdfd754f3e6e4a967e67e48d5bb125
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-23 20:17:21 -0400

    add work validation test for belongs to relationship

--- spec/models/work_spec.rb
+++ spec/models/work_spec.rb
@@ -1,7 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Work, type: :model do
+
+  let(:first_work) {
+    Work.create(
+      title: "Discourses"
+    )
+  }
+
+  let(:second_work) {
+    Work.create(
+      title: "Enchiridion"
+    )
+  }
+
   it "requires the presence of a title" do
     expect(Work.new).not_to be_valid
+  end
+
+  it "belongs to an author" do
+    author = first_work.create_author(name: "Epictetus")
+    second_work.author = author
+    expect(first_work.author).to eq(author)
+    expect(second_work.author).to eq(author)
   end
 end
