ce030106e41694026da4f23dc5a67813a4aa87c8
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-24 15:43:32 -0400

    add model test and validation for presence of body of quote

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,5 @@
 class Quote < ApplicationRecord
+    validates :body, presence: true
     belongs_to :work
     has_one :author, through: :work
     belongs_to :translator
--- spec/models/quote_spec.rb
+++ spec/models/quote_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Quote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  let(:author) {
+    Author.create(
+      name: "Epictetus"
+    )
+  }
+  
+  let(:work) {
+    Work.create(
+      title: "Enchiridion"
+    )
+  }
+
+  let(:translator) {
+    Translator.create(
+      first_name: "Stephen",
+      last_name: "Walton"
+    )
+  }
+  
+  let(:no_body_quote) {
+    Quote.new(
+      work: work,
+      translator: translator,
+      book_num: "I",
+      section_num: 2,
+    )
+  }
+  
+  it "requires a text body" do
+    work.author = author
+    expect(no_body_quote).not_to be_valid
+  end
 end
