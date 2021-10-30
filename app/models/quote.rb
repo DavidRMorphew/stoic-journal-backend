153f444c034c94657d4f36e1b9199b5ed07f34de
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-29 22:12:45 -0400

    add validation for book num to quote model

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,5 @@
 class Quote < ApplicationRecord
-    validates :body, :section_num, presence: true
+    validates :body, :section_num, :book_num, presence: true
     belongs_to :work
     has_one :author, through: :work
     belongs_to :translator
