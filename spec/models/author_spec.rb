1757fdc5ca4ed517b91a3af2fe8c2e23418998ef
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-29 22:47:47 -0400

    add author has-many quotes through works validation test

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -7,16 +7,58 @@ RSpec.describe Author, type: :model do
       name: "Epictetus"
     )
   }
+
+  let(:first_work) {
+    Work.create(
+      title: "Discourses"
+    )
+  }
+    
+  let(:second_work) {
+    Work.create(
+      title: "Enchiridion"
+    )
+  }
+
+  let(:translator) {
+    Translator.create(
+      first_name: "Stephen",
+      last_name: "Walton"
+    )
+  }
+
+  let(:first_quote) {
+    Quote.create(
+      work: first_work,
+      body: "There's a quote here",
+      translator: translator,
+      book_num: "II",
+      section_num: 1
+    )
+  }
+
+  let(:second_quote) {
+    Quote.create(
+      work: second_work,
+      body: "There's another quote here",
+      translator: translator,
+      book_num: "II",
+      section_num: 1
+    )
+  }
   
   it "requires the presence of a name" do
     expect(Author.new).not_to be_valid
   end
 
   it "has many works" do
-    first_work = Work.create(title: "Discourses")
-    second_work = Work.create(title: "Enchiridion")
     author.works << [first_work, second_work]
     expect(author.works.first).to eq(first_work)
     expect(author.works.second).to eq(second_work)
+  end
+
+  it "has many quotes through works" do
+    author.works << [first_work, second_work]
+    expect(author.quotes).to eq([first_quote, second_quote])
   end
 end
