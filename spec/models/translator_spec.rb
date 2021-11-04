12f1cc5aeddef98482dad1a6c572b58bec9823fc
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-11-04 11:52:33 -0400

    add validation test for missing attribute last_name for translator

--- spec/models/translator_spec.rb
+++ spec/models/translator_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Translator, type: :model do
   it "requires the presence of a first and last name" do
     expect(Translator.new).not_to be_valid
     expect(Translator.new(first_name: "David", last_name: "Sedley")).to be_valid
+    expect(Translator.new(first_name: "David")).not_to be_valid
   end
 end
