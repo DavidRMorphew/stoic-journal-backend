71fc963938e0dacb617b10600a5face43c744901
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-11-04 11:51:26 -0400

    add validation test with valid attributes for translator

--- spec/models/translator_spec.rb
+++ spec/models/translator_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Translator, type: :model do
   it "requires the presence of a first and last name" do
     expect(Translator.new).not_to be_valid
+    expect(Translator.new(first_name: "David", last_name: "Sedley")).to be_valid
   end
 end
