8960dc2d9a080437164eabd4f8f97481a47b344c
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-11-04 11:56:00 -0400

    refactor translator test to make translator reusable

--- spec/models/translator_spec.rb
+++ spec/models/translator_spec.rb
@@ -1,10 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Translator, type: :model do
+  
+  let(:translator) {
+    Translator.create(
+      first_name: "David", 
+      last_name: "Sedley"
+    )
+  }
+
+  let(:translator_duplicate) {
+    Translator.new(
+      first_name: "David", 
+      last_name: "Sedley"
+    )
+  }
+  
   it "requires the presence of a first and last name" do
     expect(Translator.new).not_to be_valid
-    expect(Translator.new(first_name: "David", last_name: "Sedley")).to be_valid
+    expect(translator).to be_valid
     expect(Translator.new(first_name: "David")).not_to be_valid
     expect(Translator.new(last_name: "Konstan")).not_to be_valid
+  end
+
+  it "requires the uniqueness of an translator by first and last name" do
   end
 end
