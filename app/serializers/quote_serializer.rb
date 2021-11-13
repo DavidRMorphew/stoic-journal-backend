885fdbbc71e0c10c76cb82f8293bb691f38d0452
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-11-12 20:21:15 -0500

    add id to QuoteSerializer

--- app/serializers/quote_serializer.rb
+++ app/serializers/quote_serializer.rb
@@ -1,5 +1,5 @@
 class QuoteSerializer
   include JSONAPI::Serializer
-  attributes :body, :section_num, :book_num, :translator_name, :work_name, :author_name
+  attributes :id, :body, :section_num, :book_num, :translator_name, :work_name, :author_name
   set_key_transform :camel
 end
