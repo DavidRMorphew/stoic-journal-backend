7aa82a4ed5cac0a9570474b81a7c668f6968794b
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-11-12 19:36:37 -0500

    add translator_name reader

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -4,4 +4,8 @@ class Quote < ApplicationRecord
     belongs_to :work
     has_one :author, through: :work
     belongs_to :translator
+
+    def translator_name
+        self.translator.first_name + ' ' + self.translator.last_name
+    end
 end
